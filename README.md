73641d93785ced078e2506a10b105fe58798896d
KeanuFord <KeanuF2017@gmail.com>
2024-03-14 17:26:05 -0700

    Finished the project

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# ReadMe-Gen
- Generates a readme
+
+# Read me Generator 
+
+## Description
+This is a terminal based project that accepts prompts from a user and outputs a README using the users inputs
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contribution](#contribution)
+- [Testing](#testing)
+- [Contact](#contact)
+
+## Installation
+This project requires node.js to function. Before running the program, typing npm install should install inquirer.js into the project folder
+
+## Usage
+To use this, type 'node index.js' into the terminal while within the project directory. It will ask the user to input information about the project. Once all prompts are finished, a readme will be generated in the output folder.
+
+## Licence [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+MIT
+
+## Contribution
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](https://www.contributor-covenant.org/)
+Feel free to contribute as you wish
+
+## Testing
+The readme for this project was outputed by the project itself
+
+## Contact
+Contact me at KeanuF2017@gmail.com for any questions
--- index.js
+++ index.js
@@ -1,59 +1,118 @@
+// TODO: Include packages needed for this application
 const fs = require('fs');
-const colors = require('colors');
 const inquirer = require('inquirer');
 
+// Required: 
+//  - Title
+//  - Description
+//  - Table of Contents
+//  - Installation
+//  - Usage information
+//  - License
+//  - Contribution guidelines
+//  - Test instructions
+//  - Questions (how to reach me with additional questions)
+//      - Email
 inquirer
     .prompt([
         {
             type: 'input',
-            message: 'What is your Name?',
-            name: 'name',
+            message: 'What is the title of your project',
+            name: 'title',
         },
         {
             type: 'input',
-            message: 'What city do you live in?',
-            name: 'location',
+            message: 'Describe the project',
+            name: 'description',
         },
         {
             type: 'input',
-            message: 'Say something about yourself: ',
-            name: 'bio',
+            message: 'Explain how to install',
+            name: 'install',
         },
         {
             type: 'input',
-            message: 'LinkedIn username: ',
-            name: 'linkedIn',
+            message: 'Explain how to use this:',
+            name: 'usage',
+        },
+        {
+            type: 'list',
+            message: 'How would you like this licensed?',
+            name: 'license',
+            choices: ["Apache 2.0", "Eclipse", "MIT", "Unlicense"]
         },
         {
             type: 'input',
-            message: 'Github URL: ',
-            name: 'github',
+            message: 'State the contribution guidelines:',
+            name: 'contribution',
+        },
+        {
+            type: 'input',
+            message: 'Describe the process of testing the project:',
+            name: 'test',
+        },
+        {
+            type: 'input',
+            message: 'Contact info for reaching out:',
+            name: 'contact',
         },
     ])
     .then((response) => {
-        console.log(`Name: ${response.name}`)
-        const html = 
-        `
-        <!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width", initial-scale="1.0" />
-                <title>Portfolio</title>
-            </head>
-            <body>
-                <h1>Portfolio: ${response.name}</h1>
-                <p>
-                Location: ${response.location} <br>
-                Bio: ${response.bio} <br>
-                LinkedIn: ${response.linkedIn} <br>
-                Github: ${response.github}
-            </body>
-        </html>
-        `
+        console.log(`Project: ${response.title}`)
+        let badge = "";
+        switch(response.license) {
+            case 'Apache 2.0':
+                badge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+                break;
+            case 'Eclipse':
+                badge = '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)'
+                break; 
+            case 'MIT':
+                badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+                break;
+            case 'Unlicense':
+                badge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
+                break;
+            default:
+                console.log('License error');
+        }
+        const readMe = 
+`
+# ${response.title} 
 
-        fs.writeFile('index.html', html, (err) => {
-        err ? console.error(err) : console.log('Html Created!');
+## Description
+${response.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contribution](#contribution)
+- [Testing](#testing)
+- [Contact](#contact)
+
+## Installation
+${response.install}
+
+## Usage
+${response.usage}
+
+## Licence ${badge}
+${response.license}
+
+## Contribution
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](https://www.contributor-covenant.org/)
+${response.contribution}
+
+## Testing
+${response.test}
+
+## Contact
+${response.contact}
+`
+
+        fs.writeFile(__dirname +'/output/README.md', readMe, (err) => {
+        err ? console.error(err) : console.log('ReadMe Created in output folder');
         });
 
     });
